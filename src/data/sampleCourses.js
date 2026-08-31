a4183c5bef1db3b4fd76ca925d3e3675482ef6f3
SahandMBaba <sahandmustafa005@gmail.com>
2026-08-31 14:14:22 +0300

    deploy

--- src/data/sampleCourses.js
+++ src/data/sampleCourses.js
@@ -12,17 +12,3 @@ export const PALETTE_COLORS = [
   "#14b8a6", // Teal
   "#f97316"  // Orange
 ];
-
-
-export const PALETTE_COLORS = [
-  "#6366f1", // Indigo
-  "#ec4899", // Pink
-  "#06b6d4", // Cyan
-  "#f59e0b", // Amber
-  "#10b981", // Emerald
-  "#8b5cf6", // Purple
-  "#ef4444", // Red
-  "#3b82f6", // Blue
-  "#14b8a6", // Teal
-  "#f97316"  // Orange
-];
